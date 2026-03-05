51707d9b03eda47330ce599f67dcd9c13ba85d6e
mtsiberev <m.tsiberev@gmail.com>
2026-03-06 01:45:24 +0300

    add controller

---
+++ MyStore.Api/Controllers/OrdersController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyStore.Domain.Entities;
+using MyStore.Infrastructure.Persistence;
+
+namespace MyStore.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class OrdersController(ApplicationDbContext context) : ControllerBase
+{
+    // GET: api/orders
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Order>>> GetOrders()
+    { 
+        var orders = await context.Orders
+            .Include(o => o.Items)
+            .ToListAsync();
+
+        return Ok(orders);
+    }
+
+    // POST: api/orders
+    [HttpPost]
+    public async Task<ActionResult<Order>> CreateOrder(string customerName, string product, decimal price)
+    {
+        var order = new Order { CustomerName = customerName };
+
+        order.AddItem(product, price, 1);
+
+        context.Orders.Add(order);
+        await context.SaveChangesAsync();
+
+        return CreatedAtAction(nameof(GetOrders), new { id = order.Id }, order);
+    }
+}
