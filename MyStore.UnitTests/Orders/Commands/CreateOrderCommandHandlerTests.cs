4dd0a610b008fc4d3ab3359cbbef5e185b8501b5
mtsiberev <m.tsiberev@gmail.com>
2026-03-15 23:33:26 +0300

    test: add unit tests

---
+++ MyStore.UnitTests/Orders/Commands/CreateOrderCommandHandlerTests.cs
@@ -0,0 +1,38 @@
+﻿using FluentAssertions;
+using MassTransit;
+using MyStore.Application.Common.Events;
+using MyStore.Application.Common.Interfaces;
+using MyStore.Application.Orders.Commands;
+using MyStore.Domain.Entities;
+using NSubstitute;
+
+namespace MyStore.UnitTests.Orders.Commands;
+
+public class CreateOrderCommandHandlerTests
+{
+    private readonly IOrderRepository _repository = Substitute.For<IOrderRepository>();
+    private readonly IApplicationDbContext _context = Substitute.For<IApplicationDbContext>();
+    private readonly IPublishEndpoint _publishEndpoint = Substitute.For<IPublishEndpoint>();
+    private readonly CreateOrderCommandHandler _handler;
+
+    public CreateOrderCommandHandlerTests()
+    {
+        _handler = new CreateOrderCommandHandler(_repository, _context, _publishEndpoint);
+    }
+
+    [Fact]
+    public async Task Handle_Should_CreateOrder_And_PublishEvent()
+    {
+        var command = new CreateOrderCommand("Ivan", "Laptop", 1500);
+
+        var result = await _handler.Handle(command, CancellationToken.None);
+
+        result.Should().NotBeEmpty();
+
+        await _repository.Received(1).AddAsync(Arg.Any<Order>(), Arg.Any<CancellationToken>());
+
+        await _context.Received(1).SaveChangesAsync(Arg.Any<CancellationToken>());
+
+        await _publishEndpoint.Received(1).Publish(Arg.Any<OrderCreatedEvent>(), Arg.Any<CancellationToken>());
+    }
+}
