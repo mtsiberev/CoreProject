ab66380038b016febcbba6fa68c53af0deca5a7b
mtsiberev <m.tsiberev@gmail.com>
2026-03-17 00:19:05 +0300

    feat: add cache invalidation for orders

--- MyStore.Application/Orders/Commands/CreateOrderCommandHandler.cs
+++ MyStore.Application/Orders/Commands/CreateOrderCommandHandler.cs
@@ -1,13 +1,20 @@
 ﻿using MassTransit;
 using MediatR;
+using Microsoft.Extensions.Caching.Distributed;
 using MyStore.Application.Common.Events;
 using MyStore.Application.Common.Interfaces;
 using MyStore.Application.Orders.Commands;
 using MyStore.Domain.Entities;
 
-public class CreateOrderCommandHandler(IOrderRepository repository, IApplicationDbContext context, IPublishEndpoint publishEndpoint)
+public class CreateOrderCommandHandler(
+    IOrderRepository repository, 
+    IApplicationDbContext context, 
+    IPublishEndpoint publishEndpoint,
+    IDistributedCache cache)
     : IRequestHandler<CreateOrderCommand, Guid>
 {
+
+    private const string CacheKey = "orders_list";
     public async Task<Guid> Handle(CreateOrderCommand request, CancellationToken cancellationToken)
     {
         var order = new Order { CustomerName = request.CustomerName };
@@ -15,6 +22,8 @@ public class CreateOrderCommandHandler(IOrderRepository repository, IApplication
 
         await repository.AddAsync(order, cancellationToken);
         await context.SaveChangesAsync(cancellationToken);
+
+        await cache.RemoveAsync(CacheKey, cancellationToken);
 
         await publishEndpoint.Publish(new OrderCreatedEvent(order.Id, order.CustomerName, order.TotalAmount), cancellationToken);
 
--- MyStore.Application/Orders/Queries/GetOrders/GetOrdersQuery.cs
+++ MyStore.Application/Orders/Queries/GetOrders/GetOrdersQuery.cs
@@ -1,18 +1,5 @@
-﻿using Mapster;
-using MediatR;
-using MyStore.Application.Common.Interfaces;
+﻿using MediatR;
 
 namespace MyStore.Application.Orders.Queries.GetOrders;
 
 public record GetOrdersQuery : IRequest<List<OrderDto>>;
-
-public class GetOrdersQueryHandler(IOrderRepository repository)
-    : IRequestHandler<GetOrdersQuery, List<OrderDto>>
-{
-    public async Task<List<OrderDto>> Handle(GetOrdersQuery request, CancellationToken cancellationToken)
-    {
-        var orders = await repository.GetAllAsync(cancellationToken);
-
-        return orders.Adapt<List<OrderDto>>();
-    }
-}
